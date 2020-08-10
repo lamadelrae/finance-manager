f68dc80701ae93f600fd211f202fdc986c79fe4c
Matthew Almeida <55630599+Lamadelrae@users.noreply.github.com>
2020-08-10 20:13:16 -0300

    Initial Commit

---
+++ FinanceManager/DAL/PersonDAL.cs
@@ -0,0 +1,21 @@
+﻿using FinanceManager.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.DAL
+{
+    public class PersonDAL
+    {
+        public void InsertPerson(Person Person)
+        {
+            using (var context = new FMEntities())
+            {
+                context.People.Add(Person);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ FinanceManager/Entity/PersonEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Entity
+{
+    public class PersonEntity
+    {
+        [Key]
+        public Guid UUID { get; set; }
+
+        public string PersonName { get; set; }
+
+        public int PersonAge { get; set; }
+
+        public decimal PersonMonthlyIncome { get; set; }
+
+        public byte PersonImage { get; set; }
+
+        public string Status { get; set; }
+    }
+}
