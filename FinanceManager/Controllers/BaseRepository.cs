29aedb49a68895b68a6a2adaab29cab205c0fbf9
Matthew Almeida <55630599+Lamadelrae@users.noreply.github.com>
2020-11-28 23:31:01 -0300

    Implementing repository pattern

---
+++ FinanceManager/Controllers/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinanceManager.Models.DataBase;
+using FinanceManager.Models.Interfaces;
+
+namespace FinanceManager.Controllers
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        public FinanceManagerContext Context = new FinanceManagerContext();
+
+        public virtual T GetById(int id)
+        {
+            return Context.Set<T>().Find(id);
+        }
+
+        public virtual List<T> GetAll()
+        {
+            return Context.Set<T>().ToList();
+        }
+
+        public virtual void Insert(T item)
+        {
+            Context.Set<T>().Add(item);
+            Context.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            Context.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public void Delete(T item)
+        {
+            Context.Remove(item);
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ FinanceManager/Models/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Models.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+
+        List<T> GetAll();
+
+        void Insert(T item);
+
+        void Update(T item);
+
+        void Delete(T item);
+    }
+}
