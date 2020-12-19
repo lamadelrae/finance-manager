d107b93c47f19285c33b3ef7138adf83c8c10a0d
Matthew Almeida <55630599+Lamadelrae@users.noreply.github.com>
2020-12-19 10:28:57 -0300

    fixed home bug

--- FinanceManager/Controllers/Home/HomeController.cs
+++ FinanceManager/Controllers/Home/HomeController.cs
@@ -19,17 +19,22 @@ namespace FinanceManager.Controllers
         {
             return View(GetViewModel());
         }
-        
+
         private HomeViewModel GetViewModel()
         {
             var sessionObj = SessionController.GetInstance.Session;
 
+            var billsPaid = Context.Months_Bills.ToList();
+            var incomesPaid = Context.Months_Incomes.ToList();
+            var bills = Context.Bills.Where(i => i.User_Id == sessionObj.UserId);
+            var incomes = Context.Incomes.Where(i => i.User_Id == sessionObj.UserId);
+
             return new HomeViewModel
             {
-                BillsCount = Context.Months_Bills.ToList().Count(i => i.User_Id == sessionObj.UserId),
-                IncomesCount = Context.Months_Incomes.ToList().Count(i => i.User_Id == sessionObj.UserId),
-                TotalBillsPaid = Context.Bills.Where(i => i.User_Id == sessionObj.UserId).Max(k => k.Value).ToString("C3"),
-                TotalIncomesPaid = Context.Incomes.Where(i => i.User_Id == sessionObj.UserId).Max(k => k.Value).ToString("C3")
+                BillsCount = bills.Count() > 0 ? bills.Count(i => i.User_Id == sessionObj.UserId) : 0,
+                IncomesCount = incomes.Count() > 0 ? incomes.Count(i => i.User_Id == sessionObj.UserId) : 0,
+                TotalBillsPaid = billsPaid.Count() > 0 ? billsPaid.Max(i => i.Value).ToString("C3") : "0",
+                TotalIncomesPaid = incomesPaid.Count() > 0 ? incomesPaid.Max(i => i.Value).ToString("C3") : "0"
             };
         }
     }
