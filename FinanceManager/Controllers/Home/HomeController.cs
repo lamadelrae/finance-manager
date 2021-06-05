367013ad1be8bb82c4d79d70a958ab2c87889afd
Matthew Almeida <matthew.almeida572@gmail.com>
2021-06-05 11:16:26 -0300

    fixing some code

--- FinanceManager/Controllers/Home/HomeController.cs
+++ FinanceManager/Controllers/Home/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using FinanceManager.Models.DataBase;
 using FinanceManager.Models.ViewModels;
 using FinanceManager.Controllers.Session;
+using FinanceManager.Models;
 
 namespace FinanceManager.Controllers
 {
@@ -22,17 +23,16 @@ namespace FinanceManager.Controllers
 
         private HomeViewModel GetViewModel()
         {
-            var sessionObj = WebHelpers.GetSession();
-
-            var billsPaid = Context.Months_Bills.Where(i => i.User_Id == sessionObj.UserId).ToList();
-            var incomesPaid = Context.Months_Incomes.Where(i => i.User_Id == sessionObj.UserId).ToList();
+            SessionModel session = WebHelpers.GetSession();
+            List<Months_Bills> billsPaid = Context.Months_Bills.Where(i => i.User_Id == session.UserId).ToList();
+            List<Months_Incomes> incomesPaid = Context.Months_Incomes.Where(i => i.User_Id == session.UserId).ToList();
 
             return new HomeViewModel
             {
-                BillsCount = billsPaid.Count() > 0 ? billsPaid.Count() : 0,
-                IncomesCount = billsPaid.Count() > 0 ? incomesPaid.Count() : 0,
-                TotalBillsPaid = billsPaid.Count() > 0 ? billsPaid.Max(i => i.Value).ToString("C3") : "0",
-                TotalIncomesPaid = incomesPaid.Count() > 0 ? incomesPaid.Max(i => i.Value).ToString("C3") : "0"
+                BillsCount = billsPaid.Count(),
+                IncomesCount = incomesPaid.Count(),
+                TotalBillsPaid =  billsPaid.Sum(i => i.Value).ToString("C3"),
+                TotalIncomesPaid = incomesPaid.Sum(i => i.Value).ToString("C3") 
             };
         }
     }
