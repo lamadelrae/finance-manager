bcb5907b6c08fa3de89c1b0c2a78ad697ae1072e
Lamadelrae <matthew.almeida572@gmail.com>
2021-11-09 15:56:52 -0300

    infra progress

---
+++ FinanceManager.Infrastructure.Data/DataRecords/Months/MonthExpense.cs
@@ -0,0 +1,16 @@
+﻿using FinanceManager.Infrastructure.Data.DataRecords.Base;
+using System;
+
+namespace FinanceManager.Infrastructure.Data.DataRecords.Months
+{
+    internal class MonthExpense : DataRecord
+    {
+        public Guid MonthId { get; set; }
+
+        public Guid ExpenseId { get; set; }
+
+        public decimal Value { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ FinanceManager.Infrastructure.Data/DataRecords/Months/MonthIncome.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FinanceManager.Infrastructure.Data.DataRecords.Months
+{
+    internal class MonthIncome
+    {
+        public Guid MonthId { get; set; }
+
+        public Guid IncomeId { get; set; }
+
+        public decimal Value { get; set; }
+
+        public string Description { get; set; }
+    }
+}
